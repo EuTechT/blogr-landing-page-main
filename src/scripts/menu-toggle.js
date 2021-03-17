bb77816440c7c586c1e2644a0287cc8429f1bac2
André Filipe <eutech.backup@gmail.com>
2021-03-17 13:36:35 -0300

    Uptade mobile menu: navlink-menu

--- src/scripts/menu-toggle.js
+++ src/scripts/menu-toggle.js
@@ -1,7 +1,9 @@
 const btnMenu = window.document.querySelector('#btn-menu');
 const navigation = window.document.querySelector('.navigation');
 const menuIcon = window.document.querySelector('.menu-icon');
-const navLinks = window.document.querySelectorAll('.navlink');
+//const navLinks = window.document.querySelectorAll('.navlink');
+//const listItems = window.document.querySelectorAll('.list-item');
+const navlinkMenu = window.document.querySelectorAll('.navlink-menu');
 
 btnMenu.addEventListener('click', (e) => {
     navigation.classList.toggle('navigation-active');
@@ -17,10 +19,42 @@ function changeIcon(icon) {
     }
 }
 
-navLinks.forEach((link)=>{
+/*navLinks.forEach((link)=>{
     link.addEventListener('click', (e)=>{
         const submenu = link.parentNode.querySelector('.submenu');
 
         submenu.classList.toggle('submenu-active');
+    });
+});*/
+
+/*listItems.forEach((list, i) => {
+    const parent = list.parentNode;
+    
+    if(parent.className == 'menu') {
+        list.addEventListener('click', (e) => {
+            const submenu = list.querySelector('.submenu');
+
+            submenu.classList.toggle('submenu-active');
+        });
+    }
+});*/
+
+navlinkMenu.forEach((link, i) => {
+    link.addEventListener('click', (e) => {
+        const submenus = window.document.querySelectorAll('.submenu');
+
+        submenus[i].classList.toggle('submenu-active');
+
+        if(e.target == navlinkMenu[0]) {
+            submenus[1].classList.remove('submenu-active');
+            submenus[2].classList.remove('submenu-active');
+        } else if (e.target == navlinkMenu[1]) {
+            submenus[0].classList.remove('submenu-active');
+            submenus[2].classList.remove('submenu-active');
+        } else {
+            submenus[0].classList.remove('submenu-active');
+            submenus[1].classList.remove('submenu-active');
+        }
+
     });
 });
