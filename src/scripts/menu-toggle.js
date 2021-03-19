e8a470d0a6de4153c8f9bd2525337d4ed7937b83
André Filipe <eutech.backup@gmail.com>
2021-03-19 12:38:24 -0300

    Updated mobile menu

--- src/scripts/menu-toggle.js
+++ src/scripts/menu-toggle.js
@@ -20,11 +20,17 @@ function changeIcon(icon) {
 navlinkMenu.forEach((link, index, arrayLinks) => {
     link.addEventListener('click', (event) => {
         const submenus = window.document.querySelectorAll('.submenu');
+        const imgsLinks = window.document.querySelectorAll('.img-link');
+
         submenus[index].classList.toggle('submenu--active');
+        link.classList.toggle('navlink-menu--active');
+        imgsLinks[index].classList.toggle('img-link--active');
 
         for(let i = 0; i < arrayLinks.length; i++){
             if(event.target != arrayLinks[i] && submenus[i].className == 'submenu submenu--active') {
                 submenus[i].classList.remove('submenu--active');
+                arrayLinks[i].classList.remove('navlink-menu--active');
+                imgsLinks[i].classList.remove('img-link--active');
             }
         }
     });
