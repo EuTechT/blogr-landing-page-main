e97c91965782ac8ac14cadd869310f60c218bc8f
André Filipe <eutech.backup@gmail.com>
2021-03-20 13:10:30 -0300

    desktop version started

--- src/scripts/menu-toggle.js
+++ src/scripts/menu-toggle.js
@@ -1,37 +1,29 @@
 const btnMenu = window.document.querySelector('#btn-menu');
 const navigation = window.document.querySelector('.navigation');
-/*const menuIcon = window.document.querySelector('.menu-icon');*/
 const navlinkMenu = window.document.querySelectorAll('.navlink-menu');
 
 btnMenu.addEventListener('click', (e) => {
     navigation.classList.toggle('navigation--active');
-    /*menuIcon.src = changeIcon(menuIcon.getAttribute('src'));*/
-
     btnMenu.classList.toggle('btn-menu--active');
 });
-
-/*function changeIcon(icon) {
-    if(icon == './src/images/icon-hamburger.svg') {
-        return './src/images/icon-close.svg';
-    } else {
-        return './src/images/icon-hamburger.svg';
-    }
-}*/
 
 navlinkMenu.forEach((link, index, arrayLinks) => {
     link.addEventListener('click', (event) => {
         const submenus = window.document.querySelectorAll('.submenu');
-        const imgsLinks = window.document.querySelectorAll('.img-link');
+        const imgsLinksDarks = window.document.querySelectorAll('.img-link-dark');
+        const imgsLinksLights = window.document.querySelectorAll('.img-link-light');
 
         submenus[index].classList.toggle('submenu--active');
         link.classList.toggle('navlink-menu--active');
-        imgsLinks[index].classList.toggle('img-link--active');
+        imgsLinksDarks[index].classList.toggle('img-link--active');
+        imgsLinksLights[index].classList.toggle('img-link--active');
 
         for(let i = 0; i < arrayLinks.length; i++){
             if(event.target != arrayLinks[i] && submenus[i].className == 'submenu submenu--active') {
                 submenus[i].classList.remove('submenu--active');
                 arrayLinks[i].classList.remove('navlink-menu--active');
-                imgsLinks[i].classList.remove('img-link--active');
+                imgsLinksDarks[i].classList.remove('img-link--active');
+                imgsLinksLights[i].classList.remove('img-link--active');
             }
         }
     });
@@ -44,8 +36,6 @@ window.onresize = function() {
 function closeNavigation() {
     if(window.innerWidth > 1024 && navigation.className == 'navigation navigation--active') {
         navigation.classList.remove('navigation--active');
-        /*menuIcon.src = changeIcon(menuIcon.getAttribute('src'));*/
-
         btnMenu.classList.remove('btn-menu--active');
     }
 }
